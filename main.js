25d7a6da5815ba124f0b88d9c6701c1fe7cdd304
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-13 23:04:28 -0300

    orders

--- main.js
+++ main.js
@@ -11,10 +11,28 @@ const renderItem = (item) => {
         const mealsList = document.getElementById('mealsList')
         Array.from(mealsList.children).forEach(x => x.classList.remove('selected'))
         element.classList.add('selected')
+        const mealsIdInput = document.getElementById('meals-id')
+        mealsIdInput.value = item._id
     })
     return element
 }
 window.onload = () => {
+    const orderForm = document.getElementById('order')
+    orderForm.onsubmit = (e) => {
+        e.preventDefault()
+        const mealId = document.getElementById('meals-id')
+        const mealIdValue = mealId.value
+        if (!mealIdValue) {
+            alert('Debe seleccionar algún plato')
+            return
+        }
+
+        const order = {
+            meal_id: mealIdValue,
+            user_id: 'idDeFacu'
+        }
+    }
+
     fetch('https://almuerzi.facuj.vercel.app/api/meals')
         .then(response => response.json())
         .then(data => {
