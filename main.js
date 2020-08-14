42f6556e787cf54f2328c5c1e1ee1ee058d9d324
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-14 13:05:13 -0300

    fetching and submitting orders

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let mealsState = []
+
 const stringToHTML = (s) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(s, 'text/html')
@@ -16,10 +18,18 @@ const renderItem = (item) => {
     })
     return element
 }
+const renderOrder = (order, meals) => {
+    const meal = meals.find(meal => meal._id === order.meal_id)
+    const element = stringToHTML(`<li data-id="${order._id}">${meal.name} - ${order.user_id}</li>`)
+    return element
+}
+
 window.onload = () => {
     const orderForm = document.getElementById('order')
     orderForm.onsubmit = (e) => {
         e.preventDefault()
+        const submit = document.getElementById('submit')
+        submit.setAttribute('disabled', true)
         const mealId = document.getElementById('meals-id')
         const mealIdValue = mealId.value
         if (!mealIdValue) {
@@ -31,16 +41,40 @@ window.onload = () => {
             meal_id: mealIdValue,
             user_id: 'idDeFacu'
         }
+
+        fetch('https://almuerzi.facuj.vercel.app/api/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(order)
+        }).then(x => x.json())
+            .then(respuesta => {
+                const renderedOrder = renderOrder(respuesta, mealsState)
+                const ordersList = document.getElementById('ordersList')
+                ordersList.appendChild(renderedOrder)
+                submit.removeAttribute('disabled')
+            })
     }
 
     fetch('https://almuerzi.facuj.vercel.app/api/meals')
         .then(response => response.json())
         .then(data => {
+            mealsState = data
             const mealsList = document.getElementById('mealsList')
             const submit = document.getElementById('submit')
             const listItems = data.map(renderItem)
             mealsList.removeChild(mealsList.firstElementChild)
             listItems.forEach(element => mealsList.appendChild(element))
             submit.removeAttribute('disabled')
+            fetch('https://almuerzi.facuj.vercel.app/api/orders')
+                .then(response => response.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('ordersList')
+                    const listOrders = ordersData.map(orderData => renderOrder(orderData, data))
+                    ordersList.removeChild(ordersList.firstElementChild)
+                    listOrders.forEach(element => ordersList.appendChild(element))
+                })
+
         })
 }
