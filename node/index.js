fc4cf36a749cc2a8c098216eab37f972ab61b28f
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-17 18:13:06 -0300

    final step before authentication

--- node/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('*', (request,response) => {
-    response.send({ message: 'Hola Mundo'})
-})
-
-app.listen(3000, () => console.log('El servidor está escuchando en el puerto 3000'))
